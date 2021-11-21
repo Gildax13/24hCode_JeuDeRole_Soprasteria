348716a192e27c00f45909611012a1dc9835cf28
Gildax13 <60438399+Gildax13@users.noreply.github.com>
2021-11-21 07:01:45 +0100

    Ajout commentaires

--- Defi Soin/soin.sol
+++ Defi Soin/soin.sol
@@ -6,11 +6,11 @@ contract soin is AvatarInterface{
     
     function soigner(address avatar)external view returns(int vie){
          AvatarInterface joueur = AvatarInterface(avatar);
-
+        //Récupération de la vie actuelle du personnage
         vie = joueur.getVie();
-
+        //Régénération de 10 HP
         vie = vie + 10;
-
+        //Renvoie la nouvelle valeur de la vie du personnage
         return vie;
 
     }
